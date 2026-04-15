324e629568b0de2cbfa99172d74f5367127d201e
toyajoel <toyatankwajoelsorel@gmail.com>
2026-04-15 23:47:58 +0100

    fix: configuration du service worker PWA pour forcer le popup d'installation

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('New update available');
+  },
+  onOfflineReady() {
+    console.log('App is ready for offline work');
+  },
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
