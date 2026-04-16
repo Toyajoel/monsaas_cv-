5a888dd24996f9fc14caaeac731417b04bec6b45
toyajoel <toyatankwajoelsorel@gmail.com>
2026-04-17 00:23:17 +0100

    fix: explicit rewrites for sitemap and robots + pwa inclusion

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.svg', 'robots.txt', 'sitemap.xml', 'apple-touch-icon.png'],
       manifest: {
         name: 'MonCV IA',
         short_name: 'MonCV',
